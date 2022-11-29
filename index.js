f30dd9eb19d36963834c6e6b7bd43372beb86f56
MartinKamauMaina888 <martin.maina01@student.moringaschool.com>
2022-11-29 10:47:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // code your solution here
+function saturdayFun(default_argument="roller-skate")
+
+{
+    return default_argument;
+}
+
+var default_argument=saturdayFun();
+console.log("Default Argument is:",default_argument);
+
+var passed_argument=saturdayFun("Today is on a Tuesday");
+console.log("Passed Argument is:",passed_argument);
+
+var default_argument=saturdayFun();
+console.log("Default Argument is:",default_argument);
+
+var passed_argument=saturdayFun("Today is on a Tuesday");
+console.log("Passed Argument is:",passed_argument);
+
+
+// example Two
+function wrapAdjective(default_argument ="special",visual_flair="*")
+      //let result = wrapAdjective('*')
+      //let emphatic = result("a hard worker")
+{
+
+    // return `You are ${default_argument}`;
+
+    return `You are ${visual_flair} dedicated programmer%! `;
+}
+
+    const encouragingPromptFunction = wrapAdjective("!!!")
+    console.log(encouragingPromptFunction);
